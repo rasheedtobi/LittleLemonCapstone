a4c874849c6d3ec78a9f73cb5ab4f3c6a52107d5
Rasheed Odunbaku <tobirasheed@gmail.com>
2023-07-05 16:56:51 +0200

    User authentication API settings added

--- littlemon/settings.py
+++ littlemon/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'restaurant',
     'rest_framework',
+    'rest_framework.authtoken'
 ]
     
 
@@ -129,3 +130,39 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    # 'DEFAULT_RENDERER_CLASSES': [
+    #     'rest_framework.renderers.JSONRenderer',
+    #     'rest_framework.renderers.BrowsableAPIRenderer',
+    # ],
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ),
+
+    #  'DEFAULT_FILTER_BACKENDS': [
+        
+    #     'rest_framework.filters.OrderingFilter',
+    #     'rest_framework.filters.SearchFilter',
+    # ],
+    # 'DEFAULT_PAGINATION_CLASS':
+    # 'rest_framework.pagination.PageNumberPagination',
+    # 'PAGE_SIZE': 10,
+
+    # 'DEFAULT_THROTTLE_CLASSES': [
+    #     'rest_framework.throttling.AnonRateThrottle',
+    #     'rest_framework.throttling.UserRateThrottle',
+    # ],
+
+    # 'DEFAULT_THROTTLE_RATES': {
+    #     'anon': '3/minute',
+    #     'user': '12/minute'
+    # }
+}
+
+DJOSER = {
+    "USER_ID_FIELD": "username"
+}
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
-from rest_framework.decorators import api_view
 from rest_framework import generics, status, viewsets
+from rest_framework.decorators import api_view
+from rest_framework.permissions import IsAuthenticated
 
-from .models import Menu, Booking
-from .serializers import MenuItemSerializer, BookingSerializer
+from .models import Booking, Menu
+from .serializers import BookingSerializer, MenuItemSerializer
+
+
 # Create your views here. 
 def index(request):
     return render(request, 'index.html', {})
 
 class MenuItemsView(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Menu.objects.all()
     serializer_class = MenuItemSerializer
 
@@ -20,5 +24,6 @@ class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView
     serializer_class = MenuItemSerializer
 
 class BookingViewSet(viewsets.ModelViewSet):
+   permission_classes = [IsAuthenticated]
    queryset = Booking.objects.all()
    serializer_class = BookingSerializer
